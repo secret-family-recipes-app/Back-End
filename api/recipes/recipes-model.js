d545a7646f6ead7ea41a9a06fd9743215681bfb9
sofialevin <slevin@M-4876.local>
2019-08-29 20:49:31 -0700

    Fixed join

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -11,7 +11,7 @@ module.exports = {
 async function getRecipes(userId) {
   let tags = await db('tags')
     .where({ 'recipes.user_id': userId })
-    .join('tags', 'tags.recipe_id', 'recipes.id')
+    .join('recipes', 'tags.recipe_id', 'recipes.id')
     .select('tags.tag as tags', 'tags.recipe_id');
 
   // let ingredients = await db('ingredient')
